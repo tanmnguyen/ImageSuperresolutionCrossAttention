a0b798f772375efce84dda10d8103c4608b2f452
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-03 11:54:35 -0400

    fix string error

--- train.py
+++ train.py
@@ -17,21 +17,21 @@ from models.Generator import Generator
 from models.Discriminator import Discriminator
 
 # format time to print month day year hour minute second
-result_dir = os.path.join(configs.result_dir, f"{get_time()}")
+result_dir = os.path.join(configs.result_dir, f'{get_time()}')
 os.makedirs(result_dir, exist_ok=True)
 
 # define log file 
-log_file = os.path.join(result_dir, "log.txt")
+log_file = os.path.join(result_dir, 'log.txt')
 
 def main(args):
     train_dataset = ImageSuperResDataset(
-        lr_hdf5_file=os.path.join(args.data, "lr_train.hdf5"),
-        hr_hdf5_file=os.path.join(args.data, "hr_train.hdf5")
+        lr_hdf5_file=os.path.join(args.data, 'lr_train.hdf5'),
+        hr_hdf5_file=os.path.join(args.data, 'hr_train.hdf5')
     )
 
     valid_dataset = ImageSuperResDataset(
-        lr_hdf5_file=os.path.join(args.data, "lr_valid.hdf5"),
-        hr_hdf5_file=os.path.join(args.data, "hr_valid.hdf5")
+        lr_hdf5_file=os.path.join(args.data, 'lr_valid.hdf5'),
+        hr_hdf5_file=os.path.join(args.data, 'hr_valid.hdf5')
     )
 
     train_dataloader = DataLoader(
@@ -55,24 +55,24 @@ def main(args):
     disc_optimizer = optim.Adam(disc.parameters(),lr=0.0002)
 
 
-    train_history, valid_history, opt_gen_loss = [], [], float("inf")
+    train_history, valid_history, opt_gen_loss = [], [], float('inf')
     for epoch in range(configs.epochs):
         train_history.append(train_net(train_dataloader, gen_optimizer, disc_optimizer, vgg, disc, gen))
         logging(
-            f"[Train] Epoch: {epoch + 1}/{configs.epochs} | Disc Loss: {train_history[-1]["disc_loss"]} | Gen Loss: {train_history[-1]["gen_loss"]}",
+            f'[Train] Epoch: {epoch + 1}/{configs.epochs} | Disc Loss: {train_history[-1]['disc_loss']} | Gen Loss: {train_history[-1]['gen_loss']}',
             log_file
         )
 
         valid_history.append(valid_net(valid_dataloader, vgg, disc, gen))
         logging(
-            f"[Valid] Epoch: {epoch + 1}/{configs.epochs} | Disc Loss: {valid_history[-1]["disc_loss"]} | Gen Loss: {valid_history[-1]["gen_loss"]}\n",
+            f'[Valid] Epoch: {epoch + 1}/{configs.epochs} | Disc Loss: {valid_history[-1]['disc_loss']} | Gen Loss: {valid_history[-1]['gen_loss']}\n',
             log_file
         )
 
-        if valid_history[-1]["gen_loss"] < opt_gen_loss:
-            opt_gen_loss = valid_history[-1]["gen_loss"]
-            torch.save(gen.state_dict(), os.path.join(result_dir, f"best_gen_ep{epoch}.pth"))
-            torch.save(disc.state_dict(), os.path.join(result_dir, f"best_disc_ep{epoch}.pth"))
+        if valid_history[-1]['gen_loss'] < opt_gen_loss:
+            opt_gen_loss = valid_history[-1]['gen_loss']
+            torch.save(gen.state_dict(), os.path.join(result_dir, f'best_gen_ep{epoch}.pth'))
+            torch.save(disc.state_dict(), os.path.join(result_dir, f'best_disc_ep{epoch}.pth'))
 
     # plot learning curve 
     plot_learning_curve(train_history, valid_history, result_dir)
@@ -83,12 +83,12 @@ if __name__ == '__main__':
     # parser.add_argument('-config',
     #                     '--config',
     #                     required=True,
-    #                     help="path to config file")
+    #                     help='path to config file')
     
     parser.add_argument('-data',
                         '--data',
                         required=True,
-                        help="Path to data file")
+                        help='Path to data file')
     
     args = parser.parse_args()
     main(args)
