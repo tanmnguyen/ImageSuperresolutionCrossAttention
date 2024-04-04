8e1d30cec69f940c82b3753f42d2da5622291134
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-04 14:26:15 -0400

    fixed bug valid memory issue

--- utils/steps.py
+++ utils/steps.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+import torch
 from tqdm import tqdm
 from utils.metrics.Losses import Losses
 
 def train_net(train_dataloader, gen_optimizer, disc_optimizer, vgg, disc, gen):
+    disc.train()
+    gen.train()
+
     epoch_disc_losses, epoch_gen_losses = [], []
     for lr_img, hr_img in tqdm(train_dataloader):
         disc_loss, gen_loss = Losses().calculateLossWithGrad(
@@ -24,17 +28,21 @@ def train_net(train_dataloader, gen_optimizer, disc_optimizer, vgg, disc, gen):
     }
 
 def valid_net(valid_dataloader, vgg, disc, gen):
+    disc.eval()
+    gen.eval()
+    
     epoch_disc_losses, epoch_gen_losses = [], []
     for lr_img, hr_img in tqdm(valid_dataloader):
-        disc_loss, gen_loss = Losses().calculateLoss(
-            vgg=vgg, 
-            discriminator=disc, 
-            generator=gen, 
-            LR_image=lr_img, 
-            HR_image=hr_img
-        )
-        epoch_disc_losses.append(disc_loss.detach().cpu())
-        epoch_gen_losses.append(gen_loss.detach().cpu())
+        with torch.no_grad():
+            disc_loss, gen_loss = Losses().calculateLoss(
+                vgg=vgg, 
+                discriminator=disc, 
+                generator=gen, 
+                LR_image=lr_img, 
+                HR_image=hr_img
+            )
+            epoch_disc_losses.append(disc_loss.detach().cpu())
+            epoch_gen_losses.append(gen_loss.detach().cpu())
 
     return {
         "disc_loss": np.mean(epoch_disc_losses),
