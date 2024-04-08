4c39d80c1bfd2300c5d62193a64faa986a532d75
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-08 12:59:03 -0400

    fix bug upsample

--- models/CAESRGan/Generator.py
+++ models/CAESRGan/Generator.py
@@ -21,7 +21,7 @@ class Generator(nn.Module):
 
         # upsampler 
         self.upsample1 = UpsamplePixShuffle(2, latent_dim, latent_dim)
-        self.upsample1 = UpsampleInterpolate(2, latent_dim, latent_dim)
+        self.upsample2 = UpsampleInterpolate(2, latent_dim, latent_dim)
         # self.upsample2 = UpsampleInterpolate(2, latent_dim, latent_dim)
         # self.upsample2 = UpsamplePixShuffle(2, latent_dim, latent_dim)
 
