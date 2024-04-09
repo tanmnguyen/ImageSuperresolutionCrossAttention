4871e4078a26eb7472509a9f337b350a171f61f7
Nguyen Manh Tan <tan@tanmaclocalhost.local>
2024-04-09 17:34:59 -0400

    make pixel shuffle with kernel size 3

--- models/CAESRGan/UpsamplePixShuffle.py
+++ models/CAESRGan/UpsamplePixShuffle.py
@@ -3,13 +3,11 @@ import torch.nn as nn
 class UpsamplePixShuffle(nn.Module):
     def __init__(self, upscale_factor, in_channels, out_channels):
         super().__init__()   
-        self.conv1 = nn.Conv2d(in_channels, out_channels * (upscale_factor ** 2), kernel_size=1)
-        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(in_channels, out_channels * (upscale_factor ** 2), kernel_size=3, padding=1)
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
     
     def forward(self, x):
         x = self.conv1(x)
-        # x = self.relu(x)
         x = self.pixel_shuffle(x)
         return x
         
