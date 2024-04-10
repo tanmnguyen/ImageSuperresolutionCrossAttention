e22b1e0bddeacbd1f16db62ac6d514a19d1bc8d2
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-09 22:14:47 -0400

    add icnr init function

--- models/CAESRGan/UpsamplePixShuffle.py
+++ models/CAESRGan/UpsamplePixShuffle.py
@@ -1,5 +1,18 @@
+import torch 
 import torch.nn as nn
-from icnr import ICNR
+
+def ICNR(tensor, initializer, upscale_factor=2, *args, **kwargs):
+    "tensor: the 2-dimensional Tensor or more"
+    upscale_factor_squared = upscale_factor * upscale_factor
+    assert tensor.shape[0] % upscale_factor_squared == 0, \
+        ("The size of the first dimension: "
+         f"tensor.shape[0] = {tensor.shape[0]}"
+         " is not divisible by square of upscale_factor: "
+         f"upscale_factor = {upscale_factor}")
+    sub_kernel = torch.empty(tensor.shape[0] // upscale_factor_squared,
+                             *tensor.shape[1:])
+    sub_kernel = initializer(sub_kernel, *args, **kwargs)
+    return sub_kernel.repeat_interleave(upscale_factor_squared, dim=0)
 
 class UpsamplePixShuffle(nn.Module):
     def __init__(self, upscale_factor, in_channels, out_channels):
